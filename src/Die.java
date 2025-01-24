3a7d2508d5a5754722160313db61a44096168ae7
Nathan Fox <nathanfox0102@gmail.com>
2025-01-24 08:03:27 -0600

    changes to readme.md

---
+++ src/Die.java
@@ -0,0 +1,9 @@
+public class Die {
+    public Die() {
+
+    }
+
+    public int roll() {
+        return (int)(Math.random() * 7) + 1;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Die one = new Die();
+        for(int i = 0; i < 10; i++) {
+            System.out.println(one.roll());
+        }
     }
 }
