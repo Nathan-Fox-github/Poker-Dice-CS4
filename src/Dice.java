929d15fdc841d4f2420899520f6a42aeacc92e43
Nathan Fox <nathanfox0102@gmail.com>
2025-01-25 01:27:13 -0600

    Dice class getCombo method

--- src/Dice.java
+++ src/Dice.java
@@ -1,16 +1,24 @@
 public class Dice {
     private final Die[] Dice;
+    private int[] values;
     private final int SIZE;
 
     public Dice() {
         SIZE = 5;
         this.Dice = new Die[SIZE];
+        values = new int[SIZE];
 
         Dice[0] = new Die();
         Dice[1] = new Die();
         Dice[2] = new Die();
         Dice[3] = new Die();
         Dice[4] = new Die();
+
+        values[0] = Dice[0].getCurrentValue();
+        values[1] = Dice[1].getCurrentValue();
+        values[2] = Dice[2].getCurrentValue();
+        values[3] = Dice[3].getCurrentValue();
+        values[4] = Dice[4].getCurrentValue();
     }
 
     public int[] roll(boolean[] exclude) {
@@ -23,6 +31,58 @@ public class Dice {
             }
         }
 
+        values = outcome;
         return outcome;
+    }
+
+    private int[] getCounts() {
+        int[] counts = new int[6];
+        for(int value: values) {
+            counts[value - 1]++;
+        }
+        return counts;
+    }
+
+    public String getCombo() {
+        int[] counts = getCounts();
+        int highestCombo = 0;
+
+        for(int i = 0; i < counts.length; i++) {
+            if(counts[i] == 5) highestCombo = 7; //Five of a Kind
+            else if(counts[i] == 4) highestCombo = Math.max(highestCombo, 6); //Four of a Kind
+            else if(counts[i] == 3) {
+                for (int count : counts) {
+                    if (count == 2) highestCombo = Math.max(highestCombo, 5); //Full House
+                }
+                highestCombo = Math.max(highestCombo, 3); //Three of a Kind
+            }
+            else if(counts[i] == 2) {
+                for(int j = i + 1; j < counts.length; j++) {
+                    if(counts[j] == 2) highestCombo = Math.max(highestCombo, 2); //Two Pairs
+                }
+                highestCombo = Math.max(highestCombo, 1); //One Pair
+            }
+            else if (counts[i] == 1) {
+                int seqCount = 1;
+                for(int j = i + 1; j < counts.length; j++) {
+                    if(counts[j] == 1) seqCount++;
+                    else break;
+                }
+                if(seqCount == 5) highestCombo = Math.max(highestCombo, 4); //Straight
+            }
+        }
+
+        String[] combos = new String[] {
+                "Nothing",
+                "One Pair",
+                "Two Pairs",
+                "Three of a Kind",
+                "Straight",
+                "Full House",
+                "Four of a Kind",
+                "Five of a Kind"
+        };
+
+        return combos[highestCombo];
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Player playerOne = new Player("Ishan");
+//        Player playerOne = new Player("Ishan");
         Dice dice = new Dice();
 
-        boolean[] keep = new boolean[] {false, false, false, true, true};
-        System.out.println(playerOne.getName() + " made a roll 1 of: " + Arrays.toString(dice.roll(keep)));
-        System.out.println(playerOne.getName() + " made a roll 2 of: " + Arrays.toString(dice.roll(keep)));
-        System.out.println(playerOne.getName() + " made a roll 3 of: " + Arrays.toString(dice.roll(keep)));
+        System.out.println(Arrays.toString(dice.roll(new boolean[5])) + " has the combo: " + dice.getCombo());
+        System.out.println(Arrays.toString(dice.roll(new boolean[5])) + " has the combo: " + dice.getCombo());
+        System.out.println(Arrays.toString(dice.roll(new boolean[5])) + " has the combo: " + dice.getCombo());
+        System.out.println(Arrays.toString(dice.roll(new boolean[5])) + " has the combo: " + dice.getCombo());
     }
 }
