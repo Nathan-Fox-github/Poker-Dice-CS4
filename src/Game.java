5c0f62b981e7ad8705d6816d895ad245f89760a6
Nathan Fox <nathanfox0102@gmail.com>
2025-01-26 16:16:35 -0600

    Creation of run method and game loop

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
@@ -18,20 +19,66 @@ public class Game {
 
     public void run() {
         boolean playing = true;
+        int round = 1;
+
         System.out.println("Welcome to Poker Dice!");
         System.out.println("---------------------------");
         System.out.println("Commands:");
         System.out.println("'Roll' - Rolls the dice");
         System.out.println("'1-5'  - toggle locked die");
+        System.out.println("'Cont' - End your turn");
         System.out.println("---------------------------");
 
         while(playing) {
-            for(Player currentPlayer: players) {
-                System.out.println("It is " + currentPlayer.getName() + "'s turn:");
-                String input = sc.nextLine();
-
-                if(input.equals("STOP")) playing = false;
+            System.out.println("Player Scores (round " + round + "):");
+            for(Player player: players) {
+                System.out.println(player.getName() + " - " + player.getScore());
             }
+            System.out.println("---------------------------");
+
+            for(Player currentPlayer: players) {
+                int rollCount = 1;
+
+                System.out.println("It is " + currentPlayer.getName() + "'s turn:");
+                System.out.println("Dice: " + Arrays.toString(dice.roll()) + " " + dice.getCombo());
+
+                while(rollCount < 3 && playing) {
+                    String input = sc.nextLine();
+
+                    switch (input) {
+                        case "STOP":
+                            playing = false;
+                            break;
+                        case "Roll":
+                            rollCount++;
+                            System.out.println(Arrays.toString(dice.roll()) + " " + dice.getCombo());
+                            break;
+                        case "Cont":
+                            rollCount = 3;
+                            break;
+                        default:
+                            int index = Integer.parseInt(input, 10);
+                            dice.toggleDieLock(index-1);
+                            System.out.println("Toggled die " + index + " lock");
+                            break;
+                    }
+                }
+                if(!playing) break;
+
+                String combo = dice.getCombo();
+                switch (combo) {
+                    case ("Five of a Kind") -> currentPlayer.addScore(100);
+                    case ("Four of a Kind") -> currentPlayer.addScore(50);
+                    case ("Full House") -> currentPlayer.addScore(30);
+                    case ("Three of a Kind") -> currentPlayer.addScore(20);
+                    case ("Two Pairs") -> currentPlayer.addScore(10);
+                    case ("One Pair") -> currentPlayer.addScore(5);
+                    default -> {}
+                }
+                dice.unlockAllDie();
+            }
+            round++;
+            System.out.println("---------------------------");
         }
     }
 }
